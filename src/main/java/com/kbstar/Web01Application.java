61862a02cf5e7135c3c0eed2047059f5d6ef75d2
youngeunkwon <youngnikk4929@gmail.com>
2023-04-19 13:08:51 +0900

    0419

---
+++ src/main/java/com/kbstar/Web01Application.java
@@ -0,0 +1,24 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class Web01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web01Application.class, args);
+    }
+
+    //JSP Setting
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ src/main/webapp/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 82102
+  Date: 2023-04-19
+  Time: 오전 11:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Main page</h1>
+</body>
+</html>
