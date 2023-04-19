6c8f50e4a7b5fa71820cb6bb5957d961ab9eacf8
youngeunkwon <youngnikk4929@gmail.com>
2023-04-19 17:33:48 +0900

    0419

---
+++ src/test/java/com/kbstar/Web03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
