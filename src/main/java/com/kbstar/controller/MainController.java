90e7f7079b7da4b50fb70909a0a16d2e8491d726
youngeunkwon <youngnikk4929@gmail.com>
2023-04-21 09:19:26 +0900

    0421

---
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,32 @@
+package com.kbstar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String main() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("center", "login");
+        return "index";
+    }
+
+    @RequestMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("center", "register");
+        return "index";
+    }
+
+
+    //  /quics?page=bs01    (quics:명령어, page=bs01: 데이터)
+    @RequestMapping("/quics")
+    public String quics(String page) {
+        return page;
+    }
+}
---
+++ src/test/java/com/kbstar/Web05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
