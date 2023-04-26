577c3eaf4e3cf8df5dc140e02bc604a3b11970cf
youngeunkwon <youngnikk4929@gmail.com>
2023-04-26 16:36:05 +0900

    0426

---
+++ src/test/java/com/kbstar/cust/InsertTests.java
@@ -0,0 +1,27 @@
+package com.kbstar.cust;
+
+import com.kbstar.dto.Cust;
+import com.kbstar.service.CustService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class InsertTests {
+	@Autowired
+	CustService service;
+	@Test
+	void contextLoads() throws Exception {
+		Cust obj = new Cust("id01","pwd01","james");
+		try {
+			service.register(obj);
+		}catch (Exception e){
+			log.info("등록에러...");
+			e.printStackTrace();
+		}
+
+	}
+
+}
