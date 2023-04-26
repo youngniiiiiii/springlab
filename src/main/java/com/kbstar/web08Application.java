4677a9b474abaa7993c8bc6cb359f62b53d788ba
youngeunkwon <youngnikk4929@gmail.com>
2023-04-26 09:13:46 +0900

    0426

---
+++ src/main/java/com/kbstar/web08Application.java
@@ -0,0 +1,23 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class web08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(web08Application.class, args);
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+}
---
+++ src/test/java/com/kbstar/web08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class web08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
