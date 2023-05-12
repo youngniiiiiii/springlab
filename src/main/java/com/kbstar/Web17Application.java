97003f6aed6a80e4fa908eef6438b5ceb8e2f444
youngeunkwon <youngnikk4929@gmail.com>
2023-05-12 09:18:29 +0900

    0512

---
+++ src/main/java/com/kbstar/Web17Application.java
@@ -0,0 +1,22 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class Web17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web17Application.class, args);
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
